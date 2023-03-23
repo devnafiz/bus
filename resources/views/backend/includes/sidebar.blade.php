8c8a47168afb3f8fb778dcbd58ad3daee0632de9
nafiz <nafiz.2011@yahoo.com>
2023-03-23 15:53:40 +0600

    add menu

--- resources/views/backend/includes/sidebar.blade.php
+++ resources/views/backend/includes/sidebar.blade.php
@@ -70,18 +70,24 @@
                     @endif
                 </ul>
             </li>
+           
 
+            <li class="c-sidebar-nav-dropdown ">
+                   <x-utils.link 
+                    href=""
+                    icon="c-sidebar-nav-icon cil-user" 
+                    class="c-sidebar-nav-link"
+                    :text="__('General Setting')"
+                    :active="activeClass(Route::is('admin.auth.user.*'), 'c-active')"/>
+                        
+            </li>
 
-
-
-            <li class="c-sidebar-nav-title">@lang('System')</li>
-
-            <li class="c-sidebar-nav-dropdown {{ activeClass(Route::is('admin.auth.user.*') || Route::is('admin.auth.role.*'), 'c-open c-show') }}">
+             <li class="c-sidebar-nav-dropdown {{ activeClass(Route::is('admin.auth.user.*') || Route::is('admin.auth.role.*'), 'c-open c-show') }}">
                 <x-utils.link
                     href="#"
                     icon="c-sidebar-nav-icon cil-user"
                     class="c-sidebar-nav-dropdown-toggle"
-                    :text="__('Access')" />
+                    :text="__('Manage Trips')" />
 
                 <ul class="c-sidebar-nav-dropdown-items">
                     @if (
@@ -99,7 +105,58 @@
                             <x-utils.link
                                 :href="route('admin.auth.user.index')"
                                 class="c-sidebar-nav-link"
-                                :text="__('User Management')"
+                                :text="__('Route')"
+                                :active="activeClass(Route::is('admin.auth.user.*'), 'c-active')" />
+                        </li>
+                        <li class="c-sidebar-nav-item">
+                            <x-utils.link
+                                :href="route('admin.auth.user.index')"
+                                class="c-sidebar-nav-link"
+                                :text="__('Schedule')"
+                                :active="activeClass(Route::is('admin.auth.user.*'), 'c-active')" />
+                        </li>
+                    @endif
+
+                  
+                </ul>
+            </li>
+
+
+
+
+            <li class="c-sidebar-nav-title">@lang('System')</li>
+
+            <li class="c-sidebar-nav-dropdown {{ activeClass(Route::is('admin.auth.user.*') || Route::is('admin.auth.role.*'), 'c-open c-show') }}">
+                <x-utils.link
+                    href="#"
+                    icon="c-sidebar-nav-icon cil-user"
+                    class="c-sidebar-nav-dropdown-toggle"
+                    :text="__('Manage Fleets')" />
+
+                <ul class="c-sidebar-nav-dropdown-items">
+                    @if (
+                        $logged_in_user->hasAllAccess() ||
+                        (
+                            $logged_in_user->can('admin.access.user.list') ||
+                            $logged_in_user->can('admin.access.user.deactivate') ||
+                            $logged_in_user->can('admin.access.user.reactivate') ||
+                            $logged_in_user->can('admin.access.user.clear-session') ||
+                            $logged_in_user->can('admin.access.user.impersonate') ||
+                            $logged_in_user->can('admin.access.user.change-password')
+                        )
+                    )
+                        <li class="c-sidebar-nav-item">
+                            <x-utils.link
+                                :href="route('admin.auth.user.index')"
+                                class="c-sidebar-nav-link"
+                                :text="__('Seat Layouts')"
+                                :active="activeClass(Route::is('admin.auth.user.*'), 'c-active')" />
+                        </li>
+                        <li class="c-sidebar-nav-item">
+                            <x-utils.link
+                                :href="route('admin.auth.user.index')"
+                                class="c-sidebar-nav-link"
+                                :text="__('Fleet Type')"
                                 :active="activeClass(Route::is('admin.auth.user.*'), 'c-active')" />
                         </li>
                     @endif
@@ -109,7 +166,7 @@
                             <x-utils.link
                                 :href="route('admin.auth.role.index')"
                                 class="c-sidebar-nav-link"
-                                :text="__('Role Management')"
+                                :text="__('Vehicles')"
                                 :active="activeClass(Route::is('admin.auth.role.*'), 'c-active')" />
                         </li>
                     @endif
