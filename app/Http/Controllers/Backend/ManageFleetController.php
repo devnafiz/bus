ef4632802837cb28065a274c5f4b9c900f02f45a
nafiz <nafiz.2011@yahoo.com>
2023-03-25 22:35:58 +0600

    add model

---
+++ app/Http/Controllers/Backend/ManageFleetController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\SeatLayout;
+use App\Models\FleetType;
+use App\Models\Vehicle;
+
+class ManageFleetController extends Controller
+{
+    //
+}
